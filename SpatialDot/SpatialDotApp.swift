050ab05f8920c7a0562ef43cf0ad02ad21071421
Danny Lin <danny@kdrag0n.dev>
2024-02-17 03:30:40 -0800

    Initial Commit

---
+++ SpatialDot/SpatialDotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialDotApp.swift
+//  SpatialDot
+//
+//  Created by Danny Lin on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SpatialDotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
