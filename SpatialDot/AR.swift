5467dc4b9b264907630741ac41f14c23b9be2cf8
Danny Lin <danny@kdrag0n.dev>
2024-02-18 08:14:21 -0800

    fix merge

--- SpatialDot/AR.swift
+++ SpatialDot/AR.swift
@@ -172,6 +172,7 @@ class ARClient: NSObject, ObservableObject, ARSessionDelegate, URLSessionDelegat
             case .peerClosed:
                    break
         }
+    }
     
     func advancePos() {
         
