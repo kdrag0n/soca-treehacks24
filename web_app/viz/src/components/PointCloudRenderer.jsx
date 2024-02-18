5ce207a211f426a4528669534c25be5fe5db3046
Huxley Marvit <huxley@princeton.edu>
2024-02-17 20:17:26 -0800

    update

--- web_app/viz/src/components/PointCloudRenderer.jsx
+++ web_app/viz/src/components/PointCloudRenderer.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useMemo, useState, useRef, Suspense } from 'react';
-import {  } from '@react-three/drei';
 import * as THREE from 'three';
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import circleImg from "../assets/circle.png";
+
 import { 
     Bounds,
     useBounds,
@@ -12,10 +13,63 @@ import {
     PointMaterial
 } from '@react-three/drei'
 
+const PointCloudRenderer = ({ vertices }) => {
 
-const PointCloudRenderer = () => {
+    const CircleImg = useLoader(THREE.TextureLoader, circleImg);
 
-    const NUM_POINTS = 1000*10000;
+
+    // #############
+    // load from file
+    // #############
+
+    const loadVertices = () => {
+        let positions = [];
+        import("../assets/pointcloudtest_1.json").then(arr => {
+            //console.log("hello!", arr)
+            for (let i = 0; i < arr.length; i++) {
+                const [x, y, z] = arr[i];
+                positions.push(x, y, z);
+            }
+            positions = positions.map((x) => x * 100);
+            let v = new Float32Array(positions);
+            setVertices(v);
+        });
+    }
+
+    useEffect(() => {
+        const ws = new WebSocket('ws://localhost:8000/ws/get_data');
+
+        ws.addEventListener("open", (event) => {
+            ws.send("Connection established")
+        })
+
+        ws.addEventListener("message", (event) => {
+            console.log("Message from server")
+            // expect to receive a Float32Array
+            //console.log(event.data)
+            // the event.data is a string of json which is {"float_array": [0.0, 0.0, 0.0, ...]}
+
+            let arr = JSON.parse(event.data).float_array;
+            let v = new Float32Array(arr);
+            setVertices(v);
+        })
+
+        return () => {
+            ws.close();
+        }
+    }, []);
+
+
+    useEffect(() => {
+        let v = loadVertices();
+        console.log(v);
+        //setVertices(v);
+    }, []);
+
+    // #######################
+    // test vertices
+    // #######################
+    const NUM_POINTS = 1000*100000;
     const generateVertices = () => {
         let positions = [];
         for (let xi = 0; xi <(Math.random() * NUM_POINTS); xi++) {
@@ -24,23 +78,26 @@ const PointCloudRenderer = () => {
         }
         return new Float32Array(positions);
     };
+    //const [test_vertices, setVertices] = useState(generateVertices());
+
+    //useEffect(() => {
+    //    const interval = setInterval(() => {
+
+    //        const verts = generateVertices();
+    //        setVertices(verts);
+
+    //        setKey(prevKey => prevKey + 1);
+
+    //        console.log('vertices updated');
+    //    }, 1000);
+    //    return () => clearInterval(interval);
+    //}, []);
+    // #######################
 
     const [test_vertices, setVertices] = useState(generateVertices());
+
     const [key, setKey] = useState(0);
     const positionRef = useRef();
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-
-            const verts = generateVertices();
-            setVertices(verts);
-
-            setKey(prevKey => prevKey + 1);
-
-            console.log('vertices updated');
-        }, 100);
-        return () => clearInterval(interval);
-    }, []);
 
     useEffect(() => {
         if (positionRef.current) {
@@ -53,7 +110,12 @@ const PointCloudRenderer = () => {
             className="h-screen border-0"
         >
 
-            <Canvas>
+            <Canvas
+                style={{
+                    backgroundColor: '#0B0B0B',
+                    //backgroundColor: 'black',
+                }}
+            >
                 <Bounds fit clip observe margin={1.2}>
                     <points key={key}>
                         <bufferGeometry attach="geometry">
@@ -65,16 +127,16 @@ const PointCloudRenderer = () => {
                                 itemSize={3}
                             />
                         </bufferGeometry>
-                        {/*<pointsMaterial
+                        <pointsMaterial
                             attach="material"
                             map={CircleImg}
-                            color={0x00aaff}
-                            sizes={0.5}
-                            sizeAttenuation //merupakan parameter yang menscale object berdasarkan perspective camera
+                            //color={0x00aaff}
+                            sizes={0.01}
+                            sizeAttenuation={false}
                             transparent={false}
-                            alphaTest={0.5} //merupakan thresshold saat rendering untuk mencega bila opacity dibawah value alphatest
+                            alphaTest={0.5}
                             opacity={1.0}
-                        />*/}
+                        />
                     </points>
                     <SelectToZoom>
                         <Box position={[-1.2, 0, 0]} />
